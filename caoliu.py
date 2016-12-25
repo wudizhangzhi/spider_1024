f21216fdf6b80b59411a4eef94bfe4bc7939062f
wudizhangzhi <wudizhangzhi@163.com>
2016-12-25 15:55:27 +0800

    log

--- caoliu.py
+++ caoliu.py
@@ -222,6 +222,7 @@ class CaoLiu(object):
         lines = root.xpath('//h3/a[starts-with(@href, "htm_data")]')
         if not lines:
             logging.error('该列表页面找不到内容内容:%s' % url)
+            return False
         for line in lines:
             try:
                 title = line.xpath('./text()')  # 标题
