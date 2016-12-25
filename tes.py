15f50fd04a2d75a4f87316d10e30e96dc50f8619
wudizhangzhi <wudizhangzhi@163.com>
2016-12-25 16:02:00 +0800

    log

--- tes.py
+++ tes.py
@@ -6,4 +6,4 @@ from caoliu import CaoLiu
 
 if __name__ == '__main__':
     url = 'http://www.t66y.com/thread0806.php?fid=15&search=&page=17'
-    CaoLiu.scrapylist_onepage(url)
+    CaoLiu().scrapylist_onepage(url)
